cd66e8a51297c9020b2d22a9d6501916e7aed32b
Josh Gulledge <jgulledge19@gmail.com>
2023-09-26 18:16:52 -0600

    Added about component to main

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import About from './components/About';
 
 function App() {
   return (
     <main className="text-stone-800 bg-gray-300 body-font">
       <Navbar />
+      <About />
     </main>
   );
 }
