584c8bc207adee9f335cefaf54f663939b2c1c6a
Josh Gulledge <jgulledge19@gmail.com>
2023-09-29 17:03:06 -0600

    Create data file

---
+++ src/data.js
@@ -0,0 +1,18 @@
+export const liquors = [
+    {
+        name: "Horse Soldier Bourbon",
+        image: "https://i.imgur.com/u6yQGfY.jpg",
+    },
+    {
+        name: "Dukes Whiskey",
+        image: "https://i.imgur.com/dALxKHy.jpg",
+    },
+    {
+        name: "Korbel Brandy",
+        image: "https://i.imgur.com/wjnUhpu.jpg",
+    },
+    {
+        name: "Tito's Vodka",
+        image: "https://i.imgur.com/qImRc8i.jpg",
+    },
+];
