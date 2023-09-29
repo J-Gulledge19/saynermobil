44e3cb6def2c1a9eae5a31d5ce647be37083b7f8
Josh Gulledge <jgulledge19@gmail.com>
2023-09-29 16:24:13 -0600

    Change margin on bottom

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import { ArrowRightIcon } from "@heroicons/react/solid";
 export default function Navbar() {
   return (
     <header className="bg-zinc-800 md:sticky top-0 z-10">
-      <div className="container mx-auto flex flex-wrap p-4 flex-col md:flex-row items-center">
+      <div className="container mx-auto mb-10 flex flex-wrap p-4 flex-col md:flex-row items-center">
         <h1 className="title-font font-medium text-stone-300 hover:text-blue-500 hover:scale-110 mb-4 md:mb-0">
           <a href="#about" className="ml-3 text-xl">
             Sayner Mobil
