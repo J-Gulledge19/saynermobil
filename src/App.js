1c65620b78b9c6b35d67eab8e844f970854b1ed0
Josh Gulledge <jgulledge19@gmail.com>
2023-09-29 16:25:12 -0600

    Added gas price component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import About from './components/About';
+import Gasprice from './components/Gasprice';
 
 function App() {
   return (
     <main className="text-stone-800 bg-gray-300 body-font">
       <Navbar />
       <About />
+      <Gasprice />
     </main>
   );
 }
