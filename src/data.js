4b93f59025a7ee834565031c36b8256fd8380d84
Josh Gulledge <jgulledge19@gmail.com>
2023-09-30 15:31:23 -0600

    Updated logo data

--- src/data.js
+++ src/data.js
@@ -2,17 +2,23 @@ export const liquors = [
     {
         name: "Horse Soldier Bourbon",
         image: "https://i.imgur.com/u6yQGfY.jpg",
+        price: "$59.99",
     },
     {
         name: "Dukes Whiskey",
         image: "https://i.imgur.com/dALxKHy.jpg",
+        price: "$60.99",
     },
     {
         name: "Korbel Brandy",
-        image: "https://i.imgur.com/wjnUhpu.jpg",
+        image: "https://i.imgur.com/HEwNB74.jpg",
+        price: "$40.99",
     },
     {
         name: "Tito's Vodka",
         image: "https://i.imgur.com/qImRc8i.jpg",
+        price: "$29.99",
     },
 ];
+
+export const logo = "https://i.imgur.com/VD37i1S.png"
