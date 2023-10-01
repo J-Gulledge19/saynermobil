39d40bd3e0ba4fa21805a2b4f04f8e5a4cc43bf2
Josh Gulledge <jgulledge19@gmail.com>
2023-10-01 12:03:46 -0600

    Changed some data

--- src/components/Gasprice.js
+++ src/components/Gasprice.js
@@ -4,17 +4,19 @@ export default function Gasprice() {
             <div className="container mx-auto flex px-10 py-10 pt-0 md:flex-row flex-col-reverse items-center">
                     <div className="lg:max-w-lg lg:w-full mt-10 md:w-1/2 w-5/6 mb-10 px-10 pt-10">
                         <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-black-300">Gas Prices</h1>
-                        <li className="mb-8 leading-relaxed mt-10 mb-10 ml-5 text-black-300">Regular: $2.99</li>
-                        <li className="mb-8 leading-relaxed mt-10 mb-10 ml-5 text-black-300">Mid-Grade: $3.09</li>
-                        <li className="mb-8 leading-relaxed mt-10 mb-10 ml-5 text-black-300">Premium: $3.19</li>
-                        <li className="mb-8 leading-relaxed mt-10 mb-10 ml-5 text-black-300">Diesel: $3.19</li>
+                        <li className="mb-8 leading-relaxed mt-10 mb-10 ml-5 text-black-300">Regular: $3.74</li>
+                        <li className="mb-8 leading-relaxed mt-10 mb-10 ml-5 text-black-300">Mid-Grade: $4.54</li>
+                        <li className="mb-8 leading-relaxed mt-10 mb-10 ml-5 text-black-300">Premium: $4.84</li>
                     </div>
                     <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:px-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
                     <h4 className="title-font sm:text-2xl text-3xl mb-1 font-medium text-black-300">
-                        Convenience Store
+                        Convenience/Liquor Store
                         </h4>
-                        <p className="mb-8 leading-relaxed mt-4 mb-10 ml-5 text-black-300">
-                        Our convenience store is stocked with a wide range of products to make your visit even more convenient. From snacks and beverages to automotive supplies and travel essentials, you'll find everything you need under one roof. We take pride in offering a clean and well-organized store where you can quickly find what you're looking for.
+                        <p className="mb-8 leading-relaxed mt-4 mb-2 ml-5 text-black-300">
+                        Step inside our spacious convenience store, stocked with all the essentials you may need whether you are heading to the lake, the trails, the golf course or the grill. We have a variety snacks, candy, craft sodas, and even toiletries and automotive supplies, we have everything you need to make your journey more enjoyable. 
+                        </p>
+                        <p className="mb-8 leading-relaxed mt-0 mb-10 ml-5 text-black-300">
+                        But wait, there's more! For our beverage enthusiasts, we take pride in offering an extensive selection of liquor and beer.  If you are looking for something that is allocated or high end, we have a cabinet dedicated to that.  Or look for your favorite beer in our 35 degree, well stocked Beer Cave. Whether you're searching for local brewed favorites, international brands, or your favorite domestic, you'll find the perfect beverage to complement your taste and preferences.  We even have wine, although we are convinced no one buys it. 
                         </p>
 
                         <h4 className="title-font sm:text-2xl text-3xl mb-1 font-medium text-black-300">
